f9007f708c43071ce513a446b11c52f22f9277f8
alinosratipour <alinosratipour@gmail.com>
2021-03-17 23:03:29 +0000

    fixed all bugs on the page and completed mandatory tasks

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let library = [];
 
 window.addEventListener("load", function (e) {
   populateStorage();
@@ -6,7 +6,7 @@ window.addEventListener("load", function (e) {
 });
 
 function populateStorage() {
-  if (myLibrary.length == 0) {
+  if (library.length == 0) {
     let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
     let book2 = new Book(
       "The Old Man and the Sea",
@@ -14,8 +14,8 @@ function populateStorage() {
       "127",
       true
     );
-    myLibrary.push(book1);
-    myLibrary.push(book2);
+    library.push(book1);
+    library.push(book2);
     render();
   }
 }
@@ -32,12 +32,14 @@ function submit() {
     title.value == null ||
     title.value == "" ||
     pages.value == null ||
-    pages.value == ""
+    pages.value == "" ||
+    author.value == null ||
+    author.value == ""
   ) {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
+    let book = new Book(title.value, author.value, pages.value, check.checked);
     library.push(book);
     render();
   }
@@ -54,11 +56,11 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
-  let length = myLibrary.length;
+  let length = library.length;
   for (let i = 0; i < length; i++) {
     let row = table.insertRow(1);
     let cell1 = row.insertCell(0);
@@ -66,9 +68,9 @@ function render() {
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
     let cell5 = row.insertCell(4);
-    cell1.innerHTML = myLibrary[i].title;
-    cell2.innerHTML = myLibrary[i].author;
-    cell3.innerHTML = myLibrary[i].pages;
+    cell1.innerHTML = library[i].title;
+    cell2.innerHTML = library[i].author;
+    cell3.innerHTML = library[i].pages;
 
     //add and wait for action for read/unread button
     let changeBut = document.createElement("button");
@@ -76,28 +78,36 @@ function render() {
     changeBut.className = "btn btn-success";
     cell4.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
-      readStatus = "Yes";
-    } else {
+    if (library[i].check == false) {
       readStatus = "No";
+    } else {
+      readStatus = "Yes";
     }
     changeBut.innerHTML = readStatus;
 
     changeBut.addEventListener("click", function () {
-      myLibrary[i].check = !myLibrary[i].check;
+      library[i].check = !library[i].check;
       render();
     });
 
     //add delete button to every row and render again
-    let delButton = document.createElement("button");
+    let delBut = document.createElement("button");
     delBut.id = i + 5;
     cell5.appendChild(delBut);
     delBut.className = "btn btn-warning";
     delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
-      alert(`You've deleted title: ${myLibrary[i].title}`);
-      myLibrary.splice(i, 1);
+    delBut.addEventListener("click", function () {
+      alert(`You've deleted title: ${library[i].title}`);
+      library.splice(i, 1);
       render();
     });
   }
 }
+
+//********************BUG FIXED**************************** */
+
+// 1. Delete event fixed. changed clicks to click line 97.
+// 2. myLibrary changed to library
+// 3. line 40 author.value was added to fix the bug
+// 4 line 80 and 82 readStatus changed to correct order
+// 5. missed author field validation on line 36 and 37
